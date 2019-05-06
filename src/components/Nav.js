dcea594d5495db9c4944816d58bccc08c811007e
Tyler Maxwell <tylermaxwell661@gmail.com>
2019-05-06 16:47:42 -0700

    add bootstrap

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+
+const Nav = () => {
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src="/logo.svg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />
+        {' React Bootstrap'}
+      </Navbar.Brand>
+    </Navbar>
+  );
+};
+
+export default Nav;
